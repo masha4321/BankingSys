de2225fe96ed2cd3cd3270cbd7a87d8ca093f17a
MajorMoose94 <91700406+MajorMoose94@users.noreply.github.com>
2022-03-29 21:39:25 -0400

    update

---
+++ withdraw.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Withdraw</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="maindiv">
+            <div class="success">Withdraw Amount</div>
+            <div class="col-6">
+                <?php echo $balance_log['balance'] ?>
+
+                <?php //echo $error_log['sucess'];
+                ?>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+                    <label for="amount">Amount <span class="error-msg"><span></label>
+                    <input type="text" class="input-div-nn" id="amount" name="amount" value="<?php echo $amount; ?>">
+                    <!-- <p class="error-msg"><?php echo $error_log['amount']; ?></p> -->
+
+                    <input type="submit" class="submit" value="Confirm">
+
+                    <a href="profile.php" class="href">Cancel</a>
+                    <br>
+                    <a href="index.php" class="href">Logout</a>
+                </form>
+            </div>
+            <div class="col-6"></div>
+        </div>
+    </div>
+</body>
+
+</html>
